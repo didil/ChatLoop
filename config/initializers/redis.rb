e07f42e8cc64e5b4a84bae6a58f0b45ca346624d
didil <haritahadil@gmail.com>
2013-09-16 17:45:29 -0500

    redis cloud

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
-  if ENV["REDISTOGO_URL"]
-    uri = URI.parse(ENV["REDISTOGO_URL"])
+  if ENV["REDISCLOUD_URL"]
+    uri = URI.parse(ENV["REDISCLOUD_URL"])
     REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
   end
 else
